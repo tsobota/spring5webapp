d29fcfbf9ec9b3804d36103fe521b4bb9a426e94
tsobota <mrsobota1994@gmail.com>
2020-02-24 20:07:05 +0100

    New Book Controller

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -1,5 +1,7 @@
 package guru.springframework.spring5webapp.bootstrap;
 
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -9,10 +11,14 @@ import guru.springframework.spring5webapp.repositories.PublisherRepository;
 @Component
 public class BootStrapData implements CommandLineRunner {
 
+    private final BookRepository bookRepository;
+    private final AuthorRepository authorRepository;
     private final PublisherRepository publisherRepository;
 
-    public BootStrapData(PublisherRepository publisherRepository) {
+    public BootStrapData(PublisherRepository publisherRepository, BookRepository bookRepository, AuthorRepository authorRepository) {
         this.publisherRepository = publisherRepository;
+        this.bookRepository = bookRepository;
+        this.authorRepository = authorRepository;
     }
 
     @Override
